df6f001e5ff6b48d74a4e8a95b2afa1ac478985e
ksdumont <ksdumont@gmail.com>
2020-01-29 05:57:02 -0800

    addCardForm

--- src/AddCardForm/AddCardForm.js
+++ src/AddCardForm/AddCardForm.js
@@ -11,14 +11,26 @@ export default class AddCardForm extends Component {
     handleSubmit = e => {
 
     }
+    handleTaskChange = e => {
+        const task = e.target.value;
+        this.setState({
+          task
+        });
+      };
+      handleNameChange = e => {
+        const name = e.target.value;
+        this.setState({
+          name
+        });
+      };
     render() {
         return (
             <form onSubmit={this.handleSubmit} className='add-card-form'>
             <label>Task:</label>
-            <input type='text' name='task' 
+            <input type='text' name='task' value={this.state.task} onChange={this.handleTaskChange}
                     aria-label='task' required/>
             <label>Who:</label>
-            <input type='text' name='name' 
+            <input type='text' name='name' value={this.state.name} onChange={this.handleNameChange}
                     aria-label='Your Name' required/>
             <button type='submit'>+</button>
         </form>
