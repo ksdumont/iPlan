86d381c0f6a84f47f58c017757082e917f8cdf17
ksdumont <ksdumont@gmail.com>
2020-02-11 15:05:24 -0800

    url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 8080,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://guarded-hollows-46375.herokuapp.com'
 }
